f0af0851ed9202d0cbd5978900c6513ff95bac77
David Van Loon <david.vanloon@delaget.com>
2026-08-04 09:36:04 -0500

    Automatically set window title based on repo name

--- powershell/profile.d/90-theme.ps1
+++ powershell/profile.d/90-theme.ps1
@@ -8,6 +8,10 @@ oh-my-posh init pwsh --config "$ohMyPoshThemePath" | Invoke-Expression
 function Set-PoshGitStatus {
     $global:GitStatus = Get-GitStatus
     $env:POSH_GIT_STRING = Write-GitStatus -Status $global:GitStatus
+
+    if ($global:GitStatus -and $global:GitStatus.RepoName) {
+        $Host.UI.RawUI.WindowTitle = $global:GitStatus.RepoName
+    }
 }
 
 New-Alias -Name 'Set-PoshContext' -Value 'Set-PoshGitStatus' -Scope Global -Force
